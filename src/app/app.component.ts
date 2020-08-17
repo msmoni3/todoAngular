bdf4bd3cfa9a1b861d88c8871b06f617081eae58
msmoni3 <33058946+msmoni3@users.noreply.github.com>
2020-08-17 21:49:58 +0530

    added my Todo application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
+  item="";
+  lists: any[] =[];
+  constructor( ) { }
+
+ 
+
+ removeItem(idx){
+   this.lists.splice(idx,1);
+ }
+
+   trackByFn(index, item) {
+    return index; // or item.id
+  }
+alert;
+ addToList(item){
+  
+   this.item=item;
+    if(item['todo']=="" || item['todo']==null){
+      this.alert="Please add something to list..."
+      setTimeout(() => {
+        this.alert=""
+      }, 3000);
+    }else if(this.lists.some(i=>
+      i['todo']==item['todo']
+    )){
+    
+        this.alert="You already have same task..."
+        setTimeout(() => {
+          this.alert=""
+        }, 3000);
+      
+    }else{
+      this.lists.push(item);
+
+    }
+   console.log(this.lists);
+   
+ }
+
+
 }
